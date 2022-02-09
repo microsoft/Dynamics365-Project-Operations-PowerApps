d229f4c039f793fc66304a8e43913246c1c2c6e9
Joe Boctor <joboctor@microsoft.com>
2022-02-09 10:38:48 -0800

    all schedule API stuff

---
+++ schedule-api/C#/SDK-Sample/Program.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+
+namespace ScheduelAPISamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // e.g. https://yourorg.crm.dynamics.com
+            Console.Write("Enter your environment URL: e.g.  https://myorg.crm.dynamics.com. : ");
+            string url = Console.ReadLine();
+            // e.g. you@yourorg.onmicrosoft.com
+            Console.Write("Enter your username: e.g.  myalias@mytenant.com : ");
+            string userName = Console.ReadLine(); ;
+            // e.g. y0urp455w0rd 
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            string conn = $@"
+                Url = {url};
+                AuthType = OAuth;
+                UserName = {userName};
+                Password = {password};
+                AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
+                RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+                LoginPrompt=Auto;
+                RequireNewInstance = True";
+
+            using (var svc = new CrmServiceClient(conn))
+            {
+                WhoAmIRequest request = new WhoAmIRequest();
+                WhoAmIResponse response = (WhoAmIResponse)svc.Execute(request);
+                Console.WriteLine("Your UserId is {0}", response.UserId);
+
+                ScheduleAPIHelpers scheduleAPI = new ScheduleAPIHelpers(svc);
+
+                Entity project = scheduleAPI.CreateProject();
+                project.Id = scheduleAPI.CallCreateProjectAction(project);
+                Console.WriteLine($"Project Created '{project["msdyn_subject"]}'");
+                var projectReference = project.ToEntityReference();
+
+                var teamMember = new Entity("msdyn_projectteam", Guid.NewGuid());
+                teamMember["msdyn_name"] = $"TM {DateTime.Now.ToShortTimeString()}";
+                teamMember["msdyn_project"] = projectReference;
+                var createTeamMemberResponse = scheduleAPI.CallCreateTeamMemberAction(teamMember);
+                Console.WriteLine($"Team Member Created '{teamMember["msdyn_name"]}'");
+
+
+                var description = $"My demo {DateTime.Now.ToShortTimeString()}";
+                var operationSetId = scheduleAPI.CallCreateOperationSetAction(project.Id, description);
+                Console.WriteLine($"Operation Set Created '{description}'");
+
+                var task1 = scheduleAPI.GetTask("1WW", projectReference);
+                var task2 = scheduleAPI.GetTask("2XX", projectReference, task1.ToEntityReference());
+                var task3 = scheduleAPI.GetTask("3YY", projectReference);
+                var task4 = scheduleAPI.GetTask("4ZZ", projectReference);
+
+                var assignment1 = scheduleAPI.GetResourceAssignment("R1", teamMember, task2, project);
+                var assignment2 = scheduleAPI.GetResourceAssignment("R2", teamMember, task3, project);
+
+                var task1Response = scheduleAPI.CallPssCreateAction(task1, operationSetId);
+                Console.WriteLine($"Task Created '{task1["msdyn_subject"]}'");
+                var task2Response = scheduleAPI.CallPssCreateAction(task2, operationSetId);
+                Console.WriteLine($"Task Created '{task2["msdyn_subject"]}'");
+                var task3Response = scheduleAPI.CallPssCreateAction(task3, operationSetId);
+                Console.WriteLine($"Task Created '{task3["msdyn_subject"]}'");
+                var task4Response = scheduleAPI.CallPssCreateAction(task4, operationSetId);
+                Console.WriteLine($"Task Created '{task4["msdyn_subject"]}'");
+
+                var assignment1Response = scheduleAPI.CallPssCreateAction(assignment1, operationSetId);
+                Console.WriteLine($"Assignment Created '{assignment1["msdyn_name"]}'");
+                var assignment2Response = scheduleAPI.CallPssCreateAction(assignment2, operationSetId);
+                Console.WriteLine($"Assignment Created '{assignment2["msdyn_name"]}'");
+
+                task2["msdyn_subject"] = "Updated Task";
+                var task2UpdateResponse = scheduleAPI.CallPssUpdateAction(task2, operationSetId);
+
+                project["msdyn_subject"] = $"Proj update {DateTime.Now.ToShortTimeString()}";
+                var projectUpdateResponse = scheduleAPI.CallPssUpdateAction(project, operationSetId);
+                Console.WriteLine($"Task Updated '{task2["msdyn_subject"]}'");
+
+                var task4DeleteResponse = scheduleAPI.CallPssDeleteAction(task4.Id.ToString(), task4.LogicalName, operationSetId);
+                Console.WriteLine($"Task deleted '{task4["msdyn_subject"]}'");
+
+                var assignment2DeleteResponse = scheduleAPI.CallPssDeleteAction(assignment2.Id.ToString(), assignment2.LogicalName, operationSetId);
+                Console.WriteLine($"Assignment deleted '{assignment2["msdyn_name"]}'");
+
+                var dependency1 = scheduleAPI.GetTaskDependency(project, task2, task3);
+                var dependency1Response = scheduleAPI.CallPssCreateAction(dependency1, operationSetId);
+
+                scheduleAPI.CallExecuteOperationSetAction(operationSetId);
+
+                Console.WriteLine($"Operation Set Executed");
+
+                Console.WriteLine("Done....");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ schedule-api/C#/SDK-Sample/README.md
@@ -0,0 +1,42 @@
+# ScheduleAPISamples
+This a repository to showcase our ScheduleAPI usages samples.   
+
+# Documetation
+
+1. Schedule API: https://docs.microsoft.com/en-us/dynamics365/project-operations/project-management/schedule-api-preview
+2. Microsoft Dataverse: https://docs.microsoft.com/en-us/powerapps/developer/data-platform/org-service/quick-start-org-service-console-app
+
+# Usage
+
+1. Download Visual Studio 2019
+2. Git clone https://github.com/boctorino/ScheduleAPISamples.git
+3. Open ScheduleAPISamples.sln
+4. Press F5
+5. You will be prommpted to enter the organization URL, your username and password.
+6. ![image](https://user-images.githubusercontent.com/44511390/153267219-f0aff79f-a728-4309-8ca0-fa32ef70fc01.png)
+7. Press enter once done
+
+
+
+## Contributing
+
+This project welcomes contributions and suggestions.  Most contributions require you to agree to a
+Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
+the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
+
+When you submit a pull request, a CLA bot will automatically determine whether you need to provide
+a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions
+provided by the bot. You will only need to do this once across all repos using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
+contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+## Trademarks
+
+This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft 
+trademarks or logos is subject to and must follow 
+[Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
+Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
+Any use of third-party trademarks or logos are subject to those third-party's policies.
+
